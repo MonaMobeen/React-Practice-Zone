6619ba7abacb8db14028d0fa2206d12d919f4e29
MonaMobeen <memoonamobeen612@gmmail.com>
2023-05-08 17:27:33 +0500

    Practising Web

---
+++ src/Components/Head.js
@@ -0,0 +1,5 @@
+import { Component } from "react"
+
+class head extends Components{
+    
+}
---
+++ src/Header.js
@@ -0,0 +1,6 @@
+function Header(){
+    return(
+        <h1>hello world</h1>
+    )
+}
+export default Header;
