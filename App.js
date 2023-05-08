cdd977ec047411f863e9bde31ddc2ddced4d8d4f
Memoona Mobeen <memoonamobeen618@gmail.com>
2023-05-08 17:24:13 +0500

    react zone

---
+++ App.js
@@ -0,0 +1,16 @@
+ 
+import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>Memoona </h1>
+       <Header/>
+       <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Footer.js
@@ -0,0 +1,6 @@
+function Footer(){
+    return(
+        <h2>Aneeqa</h2>
+    )
+}
+export default Footer;
